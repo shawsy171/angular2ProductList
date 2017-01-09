5accc1b7c890a892ab4e30c643d15589184b4901
Damien Shaw <damien.shaw@hotmail.co.uk>
2017-01-09 23:09:11 +0000

    first level of routing completed

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    moduleId: module.id,
+    selector: 'product-detail',
+    templateUrl: 'product-detail.component.html'  
+})
+
+export class ProductDetailComponent {
+    pageTitle: string = 'Product';
+
+
+}
